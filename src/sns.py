56098b86388e9c9bd7b92ada7f1455dca6b77083
mmacferrin <michael.macferrin@colorado.edu>
2024-04-23 12:32:36 -0600

    Adding message-attribute functionality to SNS testing.

--- src/sns.py
+++ src/sns.py
@@ -11,35 +11,55 @@ ivert_config = utils.configfile.config()
 def send_sns_message(subject: str,
                      message: str,
                      job_id: typing.Union[str, int],
-                     username: str,
-                     job_message_num: int = 0) -> None:
-    """Send a message to an AWS SNS topic."""
+                     username: str) -> dict:
+    """Send a message to an AWS SNS topic.
+
+    Args:
+        subject (str): The subject of the message.
+        message (str): The body of the message.
+        job_id (typing.Union[str, int]): The 12-digit numerical ID of the job.
+        username (str): The username of the user who submitted the job.
+
+    Returns:
+        dict: The response attributues from the SNS API.
+    """
     client = boto3.client('sns')
 
     topic_arn = ivert_config.sns_arn
     assert topic_arn is not None
 
-    # Get a message ID, so we can associate it with the job.
-    # For the deduplication of job messages, the message_id is the username, job ID and the message number.
-    # For the grouping of job messages, the group_id is the username and job ID.
-    message_id = "{0}-{1}-{2}".format(username, job_id, job_message_num)
-    group_id = "{0}-{1}".format(username, job_id)
+    # We could send "job_id" as a "Number" datatype but they max it out at 10^9 and our job_ids are 12 digits.
+    msg_attributes = {{"Name": "job_id", "Type": "String", "Value": str(job_id)},
+                      {"Name": "username", "Type": "String", "Value": username},
+                      }
 
     # Send the message.
     reply = client.publish(TopicArn=topic_arn,
                            Subject=subject,
                            Message=message,
-                           MessageDeduplicationId=message_id,
-                           MessageGroupId=group_id)
+                           MessageAttributes=msg_attributes)
 
-    print(type(reply))
-    print(reply)
     return reply
 
 
 if __name__ == "__main__":
-    send_sns_message("SNS Test email subject",
-                     "SNS Test email body",
-                     202404160000,
-                     "michael.macferrin",
-                     0)
+    username = "michael.macferrin"
+    job_id = 202404160000
+    subject = f"IVERT: Job {username}_{job_id} has been created."
+
+    message = f"""This is an automated message from the ICESat-2 Validation of Elevations Reporting Tool (IVERT). Do not reply to this message.
+
+Your job "{username}_{job_id}" has been created and is being started. You can monitor the status of your job at any time by running "ivert status {username}_{job_id}" at the command line.
+
+The following options are assigned to this job:
+[Insert stuff here for message options.]
+
+If you wish to cancel your job, run "ivert kill {username}_{job_id}" at the command line and it will be terminated when the EC2 receives the notification. All files uploaded with the job will be deleted.
+
+[Insert this bit only if results will be generated.]
+You will get another email when the job is complete and your results are ready to download."""
+
+    send_sns_message(subject,
+                     message,
+                     job_id,
+                     username)
