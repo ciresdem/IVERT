906a63ba11611e3dfd61e1a521a0be306e28ef18
mmacferrin <michael.macferrin@colorado.edu>
2024-04-29 15:51:37 -0600

    Added funcionality to detect whether a given string is a valid email address or not.

--- src/utils/is_email.py
+++ src/utils/is_email.py
@@ -14,3 +14,24 @@ def is_email(email: str) -> bool:
         bool: True if the string is a syntactically valid email address, False otherwise.
     """
     return bool(re.match(r"[^@]+@[^@]+\.[^@]+", email))
+
+
+def return_email(email: str) -> str:
+    """
+    Return the email address if it is syntactically valid.
+
+    Args:
+        email (str): The email address to check.
+
+    Returns:
+        str: The email address if it is syntactically valid.
+
+    Raises:
+        ValueError: If the email address is not syntactically valid.
+    """
+    # Strip any whitespace.
+    email = email.strip()
+    if is_email(email):
+        return email
+    else:
+        raise ValueError(f"{email} is not a valid email address.")
