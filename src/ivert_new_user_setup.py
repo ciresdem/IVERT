dbf9dc0328cbc241df927c2a092ad4c3f23777fa
mmacferrin <michael.macferrin@colorado.edu>
2024-04-29 15:50:30 -0600

    Tweaks to CLI for adding a new user.

--- src/ivert_new_user_setup.py
+++ src/ivert_new_user_setup.py
@@ -3,17 +3,20 @@
 
 import argparse
 import boto3
-import glob
+# import glob
 import os
 import re
 import sys
-import textwrap
+# import textwrap
 
-import utils.configfile
+import ivert_client_job_upload
 from utils.bcolors import bcolors
+import utils.configfile
+import utils.is_email
 
 ivert_config = utils.configfile.config()
 ivert_user_config_template = utils.configfile.config(ivert_config.ivert_user_config_template)
+
 
 def setup_new_user(args: argparse.Namespace) -> None:
     """Set up a new IVERT user on the local machine."""
@@ -33,6 +36,13 @@ def setup_new_user(args: argparse.Namespace) -> None:
     # Update the IVERT user config file.
     update_ivert_user_config(args)
 
+    # Prompt the user if requested.
+    if args.prompt:
+        confirm_inputs_with_user(args)
+
+    # Send new_user config (as an "update" command) to the IVERT cloud tool. This will subscribe the user to the IVERT SNS topic.
+    send_new_user_config_to_ivert_cloud(args)
+
     print("\nIVERT user setup complete!")
     print(f"\nYou may now run '{bcolors.BOLD}python ivert.py test{bcolors.ENDC}' to test the IVERT system.")
 
@@ -47,8 +57,44 @@ def read_ivert_s3_credentials(error_if_not_found: bool = True):
         else:
             return None
 
+
+def send_new_user_config_to_ivert_cloud(args: argparse.Namespace) -> None:
+    """Send new_user config (as an "update" command) to the IVERT cloud tool. This will subscribe the user to the IVERT SNS topic."""
+    if not args.subscribe_to_sns:
+        return
+
+    # Since the IVERT import bucket doesn't have an explicit prefix for "subscribe,", we simply upload it as
+    # an "update" command with an additional argument "--subscribe_to_sns"). This isn't in the ivert_client CLI but
+    # we fake it here.
+    # Create a copy of the original arguments
+    args_copy = argparse.Namespace(**vars(args))
+    args_copy.command = "update"
+    args_copy.ADD_NEW_USER = True
+
+    # Remove references to bucket names and access keys, as well as the local AWS profile names.
+    # The IVERT server doesn't need those and we don't need to transmit them publicly.
+    del args_copy.untrusted_bucket_name
+    del args_copy.untrusted_access_key_id
+    del args_copy.untrusted_secret_access_key
+    del args_copy.export_bucket_name
+    del args_copy.export_access_key_id
+    del args_copy.export_secret_access_key
+    del args_copy.ivert_ingest_profile
+    del args_copy.ivert_export_profile
+    # Can delete username and email since those will be grabbed from the user config file (that we just set up)
+    del args_copy.user
+    del args_copy.email
+    # The "prompt" argument is not needed for the IVERT server.
+    del args_copy.prompt
+
+    # Send the command to the IVERT cloud tool.
+    ivert_client_job_upload.upload_new_job(args_copy)
+
+
 def collect_inputs(args: argparse.Namespace, only_if_not_provided: bool = True) -> argparse.Namespace:
-    """Collect user inputs and return them as a dictionary."""
+    """Collect user inputs and return them as a dictionary.
+
+    Any arguments that were not provided at the command line will be collected from existing config and credentials files."""
 
     # Check to make sure all the args are present here.
     assert "email" in args
@@ -62,7 +108,7 @@ def collect_inputs(args: argparse.Namespace, only_if_not_provided: bool = True) 
     assert "ivert_export_profile" in args
 
     if not args.email.strip() or not only_if_not_provided:
-        args.email = input("\n" + bcolors.UNDERLINE + "Your email address" + bcolors.ENDC + " (@noaa.gov): ").strip()
+        args.email = input("\n" + bcolors.UNDERLINE + "Your email address" + bcolors.ENDC + ": ").strip()
 
     # Check for valid AWS profile names (they can basically be anyting except empty strings)
     # If we weren't provided a profile name or we aren't using the defaults, prompt for them.
@@ -92,33 +138,45 @@ def collect_inputs(args: argparse.Namespace, only_if_not_provided: bool = True) 
     args.export_access_key_id = args.export_access_key_id.strip()
     args.export_secret_access_key = args.export_secret_access_key.strip()
 
+    # Boolean flags should be strictly True/False
+    args.subscribe_to_sns = bool(args.subscribe_to_sns)
+    args.filter_sns = bool(args.filter_sns)
+
+    s3_creds_obj = None
+
     if not args.untrusted_bucket_name or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.untrusted_bucket_name = s3_creds_obj.s3_bucket_import_untrusted
 
     if not args.export_bucket_name or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.export_bucket_name = s3_creds_obj.s3_bucket_export
 
     if not args.untrusted_access_key_id or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.untrusted_access_key_id = s3_creds_obj.s3_untrusted_access_key_id
 
     if not args.untrusted_secret_access_key or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.untrusted_secret_access_key = s3_creds_obj.s3_untrusted_secret_access_key
 
     if not args.export_access_key_id or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.export_access_key_id = s3_creds_obj.s3_export_access_key_id
 
     if not args.export_secret_access_key or not only_if_not_provided:
-        s3_creds_obj = read_ivert_s3_credentials()
+        if not s3_creds_obj:
+            s3_creds_obj = read_ivert_s3_credentials()
         if s3_creds_obj is not None:
             args.export_secret_access_key = s3_creds_obj.s3_export_secret_access_key
 
@@ -128,7 +186,7 @@ def collect_inputs(args: argparse.Namespace, only_if_not_provided: bool = True) 
                          f"{ivert_config.ivert_s3_credentials_file} and run setup again. "
                          "If error persists, contact the IVERT developers.")
 
-    # Derive the user name from the email
+    # Derive the username from the email
     args.user = args.email.split("@")[0].strip().lower()
 
     return args
@@ -145,6 +203,8 @@ def validate_inputs(args: argparse.Namespace) -> None:
     assert "export_secret_access_key" in args
     assert "ivert_ingest_profile" in args
     assert "ivert_export_profile" in args
+    assert "subscribe_to_sns" in args
+    assert "filter_sns" in args
 
     # Validate a correct email address.
     email_regex = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
@@ -186,6 +246,7 @@ def validate_inputs(args: argparse.Namespace) -> None:
 
     return
 
+
 def confirm_inputs_with_user(args: argparse.Namespace) -> None:
     """
     Prompt the user to confirm the inputs.
@@ -193,23 +254,20 @@ def confirm_inputs_with_user(args: argparse.Namespace) -> None:
     print()
     print(bcolors.OKGREEN + "Please confirm the following:" + bcolors.ENDC)
 
+    headings = ("Email",
+                "Username",
+                "Untrusted bucket",
+                "Untrusted access key ID",
+                "Untrusted secret access key",
+                "Export bucket",
+                "Export access key ID",
+                "Export secret access key",
+                "Subscribe to email notifications",
+                "Filter email notifications by user",)
+
     # Get max lengths of each field:
-    max_left = max([len(k) for k in ("Email",
-                                     "Username",
-                                     "Untrusted bucket",
-                                     "Untrusted access key ID",
-                                     "Untrusted secret access key",
-                                     "Export bucket",
-                                     "Export access key ID",
-                                     "Export secret access key",)])
-    for k, v in zip(("Email",
-                     "Username",
-                     "Untrusted bucket",
-                     "Untrusted access key ID",
-                     "Untrusted secret access key",
-                     "Export bucket",
-                     "Export access key ID",
-                     "Export secret access key",),
+    max_left = max([len(k) for k in headings])
+    for k, v in zip(headings,
                     (args.email,
                      args.user,
                      args.untrusted_bucket_name,
@@ -217,14 +275,16 @@ def confirm_inputs_with_user(args: argparse.Namespace) -> None:
                      args.untrusted_secret_access_key,
                      args.export_bucket_name,
                      args.export_access_key_id,
-                     args.export_secret_access_key,)):
+                     args.export_secret_access_key,
+                     args.subscribe_to_sns,
+                     args.filter_sns,)):
         print(f"  {(k + r': ').ljust(max_left + 2)}{v}")
 
     print()
     print(bcolors.OKGREEN + bcolors.BOLD + "Is this correct?" + bcolors.ENDC + bcolors.ENDC, end="")
-    params_confirmed = (input(" [y]/n:") or "y")
+    params_confirmed = (input(" [y]/n: ") or "y")
 
-    if params_confirmed[0].lower() != "y":
+    if params_confirmed.strip()[0].lower() != "y":
         print()
         print(bcolors.FAIL + "Exiting." + bcolors.ENDC)
         sys.exit(1)
@@ -241,6 +301,7 @@ def update_local_aws_profiles(args: argparse.Namespace) -> None:
     update_local_aws_config(aws_config_file, args)
     update_local_aws_credentials(aws_credentials_file, args)
     return
+
 
 def update_local_aws_config(aws_config_file: str,
                             args: argparse.Namespace) -> None:
@@ -285,13 +346,6 @@ def update_local_aws_config(aws_config_file: str,
         else:
             config_text = re.sub(ivert_profile_search_regex, new_ivert_profile, config_text, count=1)
 
-    # print("\nOld AWS Config file contents:")
-    # print(config_text_old)
-    #
-    # print("\nNew AWS Config file contents:")
-    # print(config_text)
-    # return
-
     # Overwrite the config file.
     with open(aws_config_file, "w") as f:
         f.write(config_text)
@@ -299,6 +353,7 @@ def update_local_aws_config(aws_config_file: str,
     print(f"Updated {aws_config_file.replace(os.environ['HOME'], '~')}.")
 
     return
+
 
 def update_local_aws_credentials(aws_credentials_file: str,
                                  args: argparse.Namespace) -> None:
@@ -352,6 +407,7 @@ def update_local_aws_credentials(aws_credentials_file: str,
 
     return
 
+
 def update_ivert_user_config(args: argparse.Namespace) -> None:
     """Create or overwrite the ivert_user_config_[name].ini file."""
     # First, find all instances of existing user config files in the config/ directory.
@@ -363,18 +419,19 @@ def update_ivert_user_config(args: argparse.Namespace) -> None:
 
     # Update the user config text with the new values.
     # Update the email in the user config text.
-    user_config_text = re.sub(r"\[EMAIL_ADDRESS\]", args.email, user_config_text)
+    user_config_text = re.sub(r"user_email\s*\=\s*[\w\[\]\.\@]+", f"user_email = {args.email}", user_config_text)
 
     # Update the username in the user config text.
-    user_config_text = re.sub(r"\[USERNAME\]", args.user, user_config_text)
+    user_config_text = re.sub(r"username\s*\=\s*[\w\[\]\.\-]+", f"username = {args.user}", user_config_text)
 
     # Update the aws_profile_ivert_ingest in the user config text, if needed.
     # If it's using a different profile name, then update it.
-    if args.ivert_ingest_profile != re.search(r"(?<=aws_profile_ivert_ingest)(\s*\=\s*\w+)", user_config_text).group(1).lstrip().lstrip("=").lstrip():
-        user_config_text = re.sub(r"aws_profile_ivert_ingest\s*\=\s*\w+", f"aws_profile_ivert_ingest = {args.ivert_ingest_profile}", user_config_text)
+    user_config_text = re.sub(r"aws_profile_ivert_ingest\s*\=\s*[\w\[\]\.\-]+", f"aws_profile_ivert_ingest = {args.ivert_ingest_profile}", user_config_text)
+    user_config_text = re.sub(r"aws_profile_ivert_export\s*\=\s*[\w\[\]\.\-]+", f"aws_profile_ivert_export = {args.ivert_export_profile}", user_config_text)
 
-    if args.ivert_export_profile != re.search(r"(?<=aws_profile_ivert_export)(\s*\=\s*\w+)", user_config_text).group(1).lstrip().lstrip("=").lstrip():
-        user_config_text = re.sub(r"aws_profile_ivert_export\s*\=\s*\w+", f"aws_profile_ivert_export = {args.ivert_export_profile}", user_config_text)
+    # Write the boolean flags for the user config file. Overwrite any old ones.
+    user_config_text = re.sub(r"subscribe_to_sns\s*\=\s*[\w\[\]\.\-]+", f"subscribe_to_sns = {str(args.subscribe_to_sns)}", user_config_text)
+    user_config_text = re.sub(r"filter_sns_by_username\s*\=\s*[\w\[\]\.\-]+", f"filter_sns_by_username = {str(args.filter_sns)}", user_config_text)
 
     # Write the user config file. Overwrite any old one.
     with open(user_config_file, "w") as f:
@@ -383,6 +440,7 @@ def update_ivert_user_config(args: argparse.Namespace) -> None:
     print(f"{user_config_file} written.")
 
     return
+
 
 def get_aws_config_and_credentials_files() -> list:
     """Find the locations of the AWS config and credentials files.
@@ -401,6 +459,7 @@ def get_aws_config_and_credentials_files() -> list:
 
     return [config_file, credentials_file]
 
+
 def get_region_name_from_bucket_name(bucket_name: str) -> str:
     """Get the region name from a bucket name. The NCCF bucket names include the region name in them."""
     region_names = boto3.Session().get_available_regions("s3")
@@ -415,42 +474,58 @@ def define_and_parse_args(just_return_parser: bool=False):
     """Define and parse command-line arguments."""
 
     parser = argparse.ArgumentParser(description="Set up a new IVERT user on the local machine.")
-    parser.add_argument( "-e", "--email", dest="email", type=str, required=False, default="",
-                        help="The email address of the new user. (For now, should be firstname.lastname@noaa.gov.)")
-    parser.add_argument( "-u", "--untrusted_bucket_name", dest="untrusted_bucket_name",
+    parser.add_argument("-e", "--email", dest="email", type=utils.is_email.return_email,
+                        required=False, default="",
+                        help="The email address of the new user.")
+    parser.add_argument("-u", "--untrusted_bucket_name", dest="untrusted_bucket_name",
                         default="", type=str, required=False,
                         help="The name of the bucket where untrusted data uploaded to IVERT.")
     parser.add_argument("-uak", "--untrusted_access_key_id", dest="untrusted_access_key_id",
                         default="", type=str, required=False,
                         help="The access key ID for the bucket where untrusted data uploaded to IVERT.")
-    parser.add_argument( "-usk", "--untrusted_secret_access_key", dest="untrusted_secret_access_key",
+    parser.add_argument("-usk", "--untrusted_secret_access_key", dest="untrusted_secret_access_key",
                         default="", type=str, required=False,
                         help="The secret access key for the bucket where untrusted data uploaded to IVERT.")
-    parser.add_argument( "-xb", "--export_bucket_name", dest="export_bucket_name",
+    parser.add_argument("-xb", "--export_bucket_name", dest="export_bucket_name",
                         default="", type=str, required=False,
                         help="The name of the bucket where IVERT data is exported to be downloaded.")
-    parser.add_argument( "-xak", "--export_access_key_id", dest="export_access_key_id",
+    parser.add_argument("-xak", "--export_access_key_id", dest="export_access_key_id",
                         default="", type=str, required=False,
                         help="The access key ID for the bucket where IVERT data is exported to be downloaded.")
-    parser.add_argument( "-xsk", "--export_secret_access_key", dest="export_secret_access_key",
+    parser.add_argument("-xsk", "--export_secret_access_key", dest="export_secret_access_key",
                         default="", type=str, required=False,
                         help="The secret access key for the bucket where IVERT data is exported to be downloaded.")
-    parser.add_argument( "-ip", "--ivert_ingest_profile", dest="ivert_ingest_profile",
+    parser.add_argument("-ns", "--do_not_subscribe", dest="subscribe_to_sns", action="store_false",
+                        default=True, required=False,
+                        help="Do not subscribe the new user to the IVERT SNS topic to receive email notifications. "
+                             " This is a good option to use if you've already susbcribed to email notifications perviously and don't want to again."
+                             " Default: Subscribe to email notifications.")
+    parser.add_argument("-nf", "--no_sns_filter", dest="filter_sns", action="store_false",
+                        default=True, required=False,
+                        help="Do not filter email notifications by username. This will make you receive all emails from all jobs."
+                             " Does nothing if the -ns flag is used."
+                             " Default: Only get emails that *this* user submits.")
+    parser.add_argument("-ip", "--ivert_ingest_profile", dest="ivert_ingest_profile",
                         default=ivert_user_config_template.aws_profile_ivert_ingest,
                         type=str, required=False,
-                        help=f"The name of the AWS profile for IVERT ingest. Default: '{ivert_user_config_template.aws_profile_ivert_ingest}'.")
-    parser.add_argument( "-xp", "--ivert_export_profile", dest="ivert_export_profile",
+                        help="Manually set the name of the AWS profile for IVERT ingest. "
+                             f" Default: '{ivert_user_config_template.aws_profile_ivert_ingest}'.")
+    parser.add_argument("-xp", "--ivert_export_profile", dest="ivert_export_profile",
                         default=ivert_user_config_template.aws_profile_ivert_export,
                         type=str, required=False,
-                        help=f"The name of the AWS profile for IVERT export. Default: '{ivert_user_config_template.aws_profile_ivert_export}'.")
+                        help="Manually set the name of the AWS profile for IVERT export. "
+                             f"Default: '{ivert_user_config_template.aws_profile_ivert_export}'.")
+    parser.add_argument("-p", "--prompt", dest="prompt", default=False, action="store_true",
+                        help="Prompt the user to verify settings before uploading files to IVERT. Default: False")
 
     if just_return_parser:
         return parser
     else:
         return parser.parse_args()
 
+
 if __name__ == "__main__":
     input_args = define_and_parse_args()
 
-    # Just for local testing
+    # Just for local testing. Normally this is run as a subset menu of ivert_client.py, not standalone. But it can be run standalone.
     setup_new_user(input_args)
