59b0c438e5a92a16067b9dba532245123e0a225a
mmacferrin <michael.macferrin@colorado.edu>
2024-05-24 13:42:01 -0600

    Adding a tiny blank tiff file to use solely for testing purposes.

---
+++ src/client_test_job.py
@@ -0,0 +1,8 @@
+"""A script to run an end-to-end test job of the IVERT system, without having it actually process any data.
+
+It creates and empty .tif file, uploads it to do a validation but uses the --empty-test option to do a dry run and return a log file."""
+
+import argparse
+import os
+import sys
+import time
