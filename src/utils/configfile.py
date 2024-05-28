9d93c4cd95fd58e4893b891541bd7afce8b73105
mmacferrin <michael.macferrin@colorado.edu>
2024-05-28 12:02:33 -0600

    Fixing s3_creds attribute mismatches.

--- src/utils/configfile.py
+++ src/utils/configfile.py
@@ -257,8 +257,12 @@ class config:
                 return
             else:
                 s3_credentials = config(self.ivert_s3_credentials_file)
-                self.s3_bucket_import_untrusted = s3_credentials.s3_untrusted_bucket_name
-                self.s3_bucket_export = s3_credentials.s3_export_bucket_name
+                try:
+                    self.s3_bucket_import_untrusted = s3_credentials.s3_untrusted_bucket_name
+                    self.s3_bucket_export = s3_credentials.s3_export_bucket_name
+                except:
+                    print(dir(s3_credentials))
+                    raise
 
 
 
