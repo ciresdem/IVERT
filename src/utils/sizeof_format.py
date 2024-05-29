752ff5462aa684a2381d243a26a6c695543fa7eb
mmacferrin <michael.macferrin@colorado.edu>
2024-05-28 21:57:40 -0600

    Bugfix in sizeof_fmt to always display a whole-number of individual bytes ('55 B' rather than '55.0 B')

--- src/utils/sizeof_format.py
+++ src/utils/sizeof_format.py
@@ -8,11 +8,13 @@ def sizeof_fmt(num, suffix="B", decimal_digits=1):
     """Resturn a filesize in human readable format.
 
     Can be a number from os.path.getsize(), or a filename"""
-    if type(num) == str and os.path.exists(num):
+    if type(num) is str and os.path.exists(num):
         num = os.path.getsize(num)
 
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if abs(num) < 1024.0:
-            return f"{num:0.{decimal_digits}f} {unit}{suffix}"
+            return (f"{int(num)}" + \
+                    ('' if (unit == '') else ('{0:0.' + f'{decimal_digits}' + 'f}').format(num % 1).lstrip('0')) + \
+                    f" {unit}{suffix}")
         num /= 1024.0
     return f"{num:.1f} Yi{suffix}"
