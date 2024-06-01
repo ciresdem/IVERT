46b42f5e083058280bc63c3bd7ffa11a870bc835
mmacferrin <michael.macferrin@colorado.edu>
2024-05-31 21:10:04 -0600

    Debug print statements.

--- src/validate_dem.py
+++ src/validate_dem.py
@@ -766,6 +766,7 @@ def validate_dem_parallel(dem_name,
         dem_srs = osr.SpatialReference(wkt=dem_proj_wkt)
 
         # DEBUG TODO: REMOVE LATER
+        print(dem_ds, type(dem_ds))
         print("icesat2_srs:", icesat2_srs)
         print("dem_proj_wkt:", dem_proj_wkt)
         print("dem_srs", dem_srs)
