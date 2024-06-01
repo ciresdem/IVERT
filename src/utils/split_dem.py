4044c30331edd283a263f37a66fe696b252287c3
mmacferrin <michael.macferrin@colorado.edu>
2024-06-01 14:33:04 -0600

    Added utility for splitting a DEM into NxN equal sub-files.

---
+++ src/utils/split_dem.py
@@ -0,0 +1,114 @@
+"""Quick utility for splitting a large DEM into sub-segments to ease processing constraints."""
+
+import argparse
+import glob
+import itertools
+import os
+import subprocess
+import shlex
+import sys
+import typing
+from osgeo import gdal
+
+if vars(sys.modules[__name__])['__package__'] == 'ivert_utils':
+    import ivert.s3 as s3
+else:
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    import s3
+
+gdal.UseExceptions()
+
+def split(dem_name: str,
+          factor: int = 2,
+          output_dir: typing.Union[str, None]=None,
+          verbose: bool = True) -> list[str]:
+    """Split a DEM into sub-segments, each side split by a factor. 2 will create 4 sub-segments.
+
+    Args:
+        dem_name (str): The name of the DEM, with path.
+        factor (int): The factor by which to split the DEM.
+        output_dir (str): The directory to which the sub-segments will be written. Defaults to the same directory as the DEM.
+        verboase (bool): Whether to print messages.
+
+    Returns:
+        list[str]: The names of the new DEM files.
+        """
+
+    if output_dir is None:
+        output_dir = os.path.dirname(dem_name)
+
+    Y, X = gdal.Open(dem_name, gdal.GA_ReadOnly).ReadAsArray().shape
+    # How to cover all the DEM when it can't be split evenly?
+    X_steps = evenly_split(X, factor)
+    Y_steps = evenly_split(Y, factor)
+
+    for xi, xb in zip(range(factor), X_steps):
+        for yj, yb in zip(range(factor), Y_steps):
+            assert len(xb) == 2
+            assert len(yb) == 2
+            fn_out = os.path.join(output_dir, f"{os.path.splitext(os.path.basename(dem_name))[0]}_{yj}.{xi}.tif")
+
+            if os.path.exists(fn_out):
+                print(fn_out, "already exists.")
+                continue
+
+            gdal_cmd = f"""gdal_translate -of GTiff
+                          -srcwin {xb[0]} {yb[0]} {xb[1] - xb[0] + 1} {yb[1] - yb[0] + 1}
+                          -co COMPRESS=DEFLATE -co PREDICTOR=2 -co TILED=YES
+                          {repr(dem_name)} {repr(fn_out)}"""
+
+            gdal_args = shlex.split(gdal_cmd.replace("\n", " "))
+            print(" ".join(gdal_args))
+
+            subprocess.run(gdal_args)
+
+            if os.path.exists(fn_out):
+                if verbose:
+                    print(fn_out, "written.")
+            else:
+                if verbose:
+                    print(fn_out, "failed.")
+
+    return []
+
+
+def evenly_split(N: int, factor: int) -> list:
+    """Split N evenly into factor pieces by index.
+
+    If it doesn't split evenly, add an extra to the last (remainder) pieces to make it as even as possible.
+
+    Returns the starting and ending index of each sub-segment.
+    """
+    batches_all = list(itertools.batched(range(N), N // factor))
+    if len(batches_all) == factor:
+        batches = [(b[0], b[-1]) for b in batches_all]
+    else:
+        assert len(batches_all) == (factor + 1)
+        batches = [(b[0], b[-1]) for b in batches_all[:-1]]
+        extras = batches_all[-1]
+        assert len(extras) < len(batches)
+
+        M = len(extras)
+        for i in range(M):
+            j = -(i + 1)
+            batches[j] = (batches[j][0] + (M + j), batches[j][1] + (M + j + 1))
+
+        assert len(batches) == factor
+
+    return batches
+
+def define_and_parse_args():
+    parser = argparse.ArgumentParser(description="Split a DEM into sub-segments, each side split by a factor.")
+    parser.add_argument("dem_name",
+                        help="The name of the DEM file. May use bash-style glob flags (*.tif) to select multiple files.")
+    parser.add_argument("-f", "--factor", type=int, default=2,
+                        help="The factor by which to split each side of the DEM. This will create f^2 files.")
+    parser.add_argument("-o", "--output_dir", type=str, default=None,
+                        help="The directory to which the sub-segments will be written. Default: will use the same directory as the input DEM.")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = define_and_parse_args()
+    split(args.dem_name, args.factor, args.output_dir)
