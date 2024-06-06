44dae6100beac6d40287eae132912da1a0bc2b7f
mmacferrin <michael.macferrin@colorado.edu>
2024-06-06 11:25:19 -0600

    Adding exception handling to is_aws() to elegantly exit if the environment is actively being shut down.

--- src/utils/is_aws.py
+++ src/utils/is_aws.py
@@ -10,7 +10,14 @@ def is_aws():
 
     # This logic is for checking for an EC2 instance. We may need to look for certain environment variables if we're
     # running in AWS Lamba functions, or similar. Cross that bridge when it comes.
-    return os.path.exists(datasource_path) and "DataSourceEc2" in open(datasource_path, 'r').read()
+    try:
+        return os.path.exists(datasource_path) and "DataSourceEc2" in open(datasource_path, 'r').read()
+
+    except NameError:
+    except FileNotFoundError:
+        # During process shutdown, as the process is no longer running we can hit an error here. Just return False if
+        # if that happens.
+        return False
 
 
 if __name__ == "__main__":
