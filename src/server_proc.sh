64bc9880f8ac335c622948bb82f13d7505269696
mmacferrin <michael.macferrin@colorado.edu>
2024-08-02 17:17:42 -0600

    Added 'restart' and 'help' options to server_proc.sh

--- src/server_proc.sh
+++ src/server_proc.sh
@@ -3,10 +3,11 @@
 # To end/kill the server, add the -k flag: "server_proc.sh -k"
 # To list the process running, add the -l flag: "server_proc.sh -l"
 
-while getopts "kl" OPTION;
+while getopts "klrh" OPTION;
 do
   case "$OPTION" in
     k)
+      # Kill the server
       echo "pkill -e -f 'python3 maintain_server_manager.py'"
       pkill -e -f "python3 maintain_server_manager.py"
       echo "pkill -e -f 'python3 ivert_server_job_manager.py'"
@@ -14,7 +15,29 @@ do
       exit 0
       ;;
     l)
+      # List the running server processes
       pgrep -a python3 | grep -e maintain_server_manager\.py -e ivert_server_job_manager\.py
+      exit 0
+      ;;
+    r)
+      # Restart the server
+      echo "pkill -e -f 'python3 maintain_server_manager.py'"
+      pkill -e -f "python3 maintain_server_manager.py"
+      echo "pkill -e -f 'python3 ivert_server_job_manager.py'"
+      pkill -e -f "python3 ivert_server_job_manager.py"
+      # Echo the command back to the user.
+      echo "nohup python3 maintain_server_manager.py -v >> /mnt/uvol0/ivert_data/ivert_server.log 2>&1 <&- &"
+      # Start the server
+      nohup python3 maintain_server_manager.py -v >> /mnt/uvol0/ivert_data/ivert_server.log 2>&1 <&- &
+      exit 0
+      ;;
+    h)
+      # Print a help message.
+      echo "Usage: server_proc.sh [-klrh]"
+      echo "  -k: Kill the server"
+      echo "  -l: List the running server processes"
+      echo "  -r: Restart the server"
+      echo "  -h: Print this help message"
       exit 0
       ;;
     *)
@@ -24,6 +47,7 @@ do
   esac
 done
 
+# By default, start the server.
 # Echo the command back to the user.
 echo "nohup python3 maintain_server_manager.py -v >> /mnt/uvol0/ivert_data/ivert_server.log 2>&1 <&- &"
 # Start the server
