927cba8ab96fcff9f8813c8bf74827fdec47bfe3
mmacferrin <michael.macferrin@colorado.edu>
2024-08-02 16:46:49 -0600

    Trying to fix import bug in jobs_database.py

--- src/jobs_database.py
+++ src/jobs_database.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
 ## A module for managing the IVERT jobs database.
 
 import argparse
 import botocore.exceptions
 import dateparser
-import datetime
+# import datetime
 import os
 import pandas
 import re
@@ -14,7 +13,6 @@ import sys
 import time
 import typing
 
-try:
     if vars(sys.modules[__name__])['__package__'] in ('ivert', 'ivert_utils'):
         # When this is built a setup.py package, it names the modules 'ivert' and 'ivert_utils'. This reflects that.
         import ivert_utils.configfile as configfile
@@ -22,13 +20,16 @@ try:
         import ivert_utils.version as version
         import ivert.s3 as s3
     else:
-        import utils.configfile as configfile
-        import utils.version as version
-        import utils.version_check_server as version_check_server
-        import s3
-except ModuleNotFoundError as e:
-    print(vars(sys.modules[__name__])['__package__'])
-    raise e
+        try:
+            import utils.configfile as configfile
+            import utils.version as version
+            import utils.version_check_server as version_check_server
+            import s3
+        except ModuleNotFoundError as e:
+            import ivert_utils.configfile as configfile
+            import ivert_utils.version_check_server as version_check_server
+            import ivert_utils.version as version
+            import ivert.s3 as s3
 
 ivert_config = configfile.config()
 
