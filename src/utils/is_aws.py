f52706f58a2a82c444d5f77a8b8101a3b0187edb
mmacferrin <michael.macferrin@colorado.edu>
2024-08-07 14:12:08 -0600

    Fixing comment typos.

--- src/utils/is_aws.py
+++ src/utils/is_aws.py
@@ -3,13 +3,12 @@ import os
 
 def is_aws():
     "Return True if running in an Amazon Web Services environment. False otherwise."
-
     # The Amazon OS 2 EC2 instances we run have a /var/lib/cloud/instance/datasource file,
-    # which contains "DataSourceEc2: DataSouceEc2" line. Look for that.
+    # which contains "DataSourceEc2: DataSourceEc2" line. Look for that.
     datasource_path = "/var/lib/cloud/instance/datasource"
 
     # This logic is for checking for an EC2 instance. We may need to look for certain environment variables if we're
-    # running in AWS Lamba functions, or similar. Cross that bridge when it comes.
+    # running in AWS Lambda functions, or similar. Cross that bridge when it comes.
     try:
         return os.path.exists(datasource_path) and "DataSourceEc2" in open(datasource_path, 'r').read()
 
