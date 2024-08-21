7ce3281f5dcfee47abd85bf0a87c82b7dfc05147
mmacferrin <michael.macferrin@colorado.edu>
2024-08-21 13:20:03 -0600

    Updating the client job status outputs to be a bit more user-friendly.

--- src/client_job_status.py
+++ src/client_job_status.py
@@ -90,10 +90,10 @@ def run_job_status_command(args: argparse.Namespace) -> None:
     if args.detailed:
         job_df, files_df = detailed_job_info(args.job_name, jobs_db)
         if len(job_df) == 0:
-            print(f"Job {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} has not started on the IVERT server yet.")
+            print(f"\nJob {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} has not started on the IVERT server yet.")
             return
 
-        print(f"Job {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} is {bcolors.BOLD}{repr(job_df['status'].values[0])}{bcolors.ENDC}.")
+        print(f"\nJob {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} is {bcolors.BOLD}{repr(job_df['status'].values[0])}{bcolors.ENDC}.")
         input_files = files_df[files_df["import_or_export"].isin((0, 2))]
         export_files = files_df[files_df["import_or_export"].isin((1, 2))]
 
@@ -144,11 +144,11 @@ def run_job_status_command(args: argparse.Namespace) -> None:
     else:
         status = get_simple_job_status(args.job_name, jobs_db)
         if status is None:
-            print(f"Job {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} has not started on the IVERT server yet.")
+            print(f"\nJob {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} has not started on the IVERT server yet.")
             # \n"
             #       "Give it a bit. If it never shows up, contact your IVERT administrator and check whether the server process is running.")
         else:
-            print(f"Job {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} is {bcolors.BOLD}{repr(status)}{bcolors.ENDC}.")
+            print(f"\nJob {bcolors.OKBLUE}{bcolors.BOLD}{args.job_name}{bcolors.ENDC}{bcolors.ENDC} is {bcolors.BOLD}{repr(status)}{bcolors.ENDC}.")
 
 
 def get_simple_job_status(job_name, jobs_db: typing.Union[jobs_database.JobsDatabaseClient, None] = None) -> typing.Union[str, None]:
