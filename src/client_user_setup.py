44d5dad80eae89db7522b7f682ef346c26a69607
mmacferrin <michael.macferrin@colorado.edu>
2024-09-25 12:02:30 -0600

    Fixing parameter issues and removing print statements for new user setup.

--- src/client_user_setup.py
+++ src/client_user_setup.py
@@ -492,8 +492,6 @@ def update_local_aws_config(aws_config_file: str,
             [(args.ivert_import_profile, args.untrusted_bucket_name, args.untrusted_endpoint_url),
              (args.ivert_export_profile, args.export_bucket_name, args.export_endpoint_url)]:
 
-        print(f"Updating profile {profile_id_string}...\n")
-
         # Identify if old IVERT profile names are being used here.
         if '[profile ivert_ingest]' in config_text and profile_id_string == args.ivert_import_profile:
             old_ivert_profile_string = '[profile ivert_ingest]'
@@ -522,8 +520,6 @@ def update_local_aws_config(aws_config_file: str,
             config_text = str(config_text.rstrip("\n\r ")) + "\n\n" + new_ivert_profile
         else:
             config_text = re.sub(old_ivert_profile_search_regex, new_ivert_profile, config_text, count=1)
-
-        print(config_text, "\n")
 
     # Get rid of excess newlines that may have accidentally been added in the config file.
     config_text = config_text.rstrip("\n\r ").lstrip("\n\r ")
