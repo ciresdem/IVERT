79b1981cfb8c3fcb1275c4d1b75d4c675e57dfc4
mmacferrin <michael.macferrin@colorado.edu>
2024-10-07 09:20:43 -0600

    Debugging export client fetch_min_client_from_server a bit more.

--- src/utils/version_check_client.py
+++ src/utils/version_check_client.py
@@ -39,12 +39,12 @@ def fetch_min_client_from_server(ivert_config=None):
     print("aws_profile_name", profile_name)
 
     # Fetch the version from the server database. Not using s3.py to avoid circular imports.
-    if endpoint_url:
-        print("Got here 1")
-        client = boto3.Session(profile_name=profile_name).client('s3', endpoint_url=endpoint_url)
-    else:
+    if endpoint_url is None:
         print("Got here 2")
         client = boto3.Session(profile_name=profile_name).client('s3')
+    else:
+        print("Got here 1")
+        client = boto3.Session(profile_name=profile_name).client('s3', endpoint_url=endpoint_url)
 
     if ivert_config.ivert_export_client_use_aws_tags_instead_of_metadata:
         tagset = client.get_object_tagging(Bucket=str(ivert_config.s3_bucket_export_client),
